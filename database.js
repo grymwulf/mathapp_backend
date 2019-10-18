9483b76e10ecc9f3cc98f59ed1de6f5c6c3efbd9
grymwulf <shawn@grymwulf.com>
2019-10-18 11:25:03 -0700

    Update database.js
    Correct type

--- database.js
+++ database.js
@@ -21,7 +21,7 @@ const TestModel = require('./models/test');
 
 // instantiate database AWS
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-    host: process.env.DB_ADDRESS
+    host: process.env.DB_ADDRESS,
     dialect: process.env.DB_DIALECT
 });
 
