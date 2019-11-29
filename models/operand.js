8babf16ae0e2e06fbb923ac60b50f189364401d8
Raymond <raymond.acevedo5@gmail.com>
2019-11-29 00:27:09 -0500

    Create operand.js

---
+++ models/operand.js
@@ -0,0 +1,30 @@
+/*
+    Copyright 2019 SER401 Project 14 Team - All Rights Reserved
+
+    Team Members: 
+    RAYMOND ACEVEDO
+    SHAWN WEINER
+    CHRISTOPHER SALAZAR
+    ROBERT PILLITTERI
+    SHELTON LACY 
+
+    Unauthorized copying of this file, via any medium is strictly prohibited
+    Proprietary and confidential
+*/
+
+const Sequelize = require('sequelize');
+
+module.exports = (sequelize, type) => {
+    return sequelize.define('operand', {
+
+        // id: primary ID for the test
+        id: {
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+
+        // implement a data field to store data for testing get/post
+        data: Sequelize.TEXT
+    })
+}
