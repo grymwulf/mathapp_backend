2a30f6a9187bcf9bf882eaedce9891b4c1bb31b1
Christopher Salazar <csalaz14@asu.edu>
2020-01-17 16:54:53 -0800

    Find test based on student id

--- routes/tests.js
+++ routes/tests.js
@@ -46,6 +46,65 @@ router.get('/:id', function(req,res) {
         })
 });
 
+// basic getter to get record by teacher id
+router.get('/:id', function(req,res) {
+    var result = {};
+    data.Test.findAll({
+            where: {
+                id: req.params.id
+            }
+        })
+        .then( testData => {
+            result['data'] = testData;
+            result['endpoint'] = `/tests/:teacher`;
+            result['responseCode'] = HttpStatus.OK;
+            result['response'] = "Query Successful";
+            res.status(result.responseCode);
+            res.json(result);
+            return;
+        }).catch(function (err) {
+            console.log('Error querying a student');
+            console.log(err)
+            result['data'] = {};
+            result['endpoint'] = `/tests/:teacher`;
+            result['responseCode'] = HttpStatus.INTERNAL_SERVER_ERROR;
+            result['response'] = "Internal Server Error";
+            res.status(result.responseCode);
+            res.json(result);
+            return;
+        })
+});
+
+// basic getter to get record by teacher id
+router.get('/:id', function(req,res) {
+    var result = {};
+    data.Test.findAll({
+            where: {
+                id: req.params.id
+            }
+        })
+        .then( testData => {
+            result['data'] = testData;
+            result['endpoint'] = `/tests/:student`;
+            result['responseCode'] = HttpStatus.OK;
+            result['response'] = "Query Successful";
+            res.status(result.responseCode);
+            res.json(result);
+            return;
+        }).catch(function (err) {
+            console.log('Error querying a student');
+            console.log(err)
+            result['data'] = {};
+            result['endpoint'] = `/tests/:student`;
+            result['responseCode'] = HttpStatus.INTERNAL_SERVER_ERROR;
+            result['response'] = "Internal Server Error";
+            res.status(result.responseCode);
+            res.json(result);
+            return;
+        })
+});
+
+
 // implementing a basic getter to get all known tests in the DB
 router.get('/', function (req, res) {
     var result = {};
