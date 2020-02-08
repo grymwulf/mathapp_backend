f8d3740a93a2a6bbc76c7372800f3fc5232994c9
Raymond <raymond.acevedo5@gmail.com>
2020-02-08 06:20:53 -0600

    Update test.js

--- models/test.js
+++ models/test.js
@@ -1,13 +1,11 @@
 /*
     Copyright 2019 SER401 Project 14 Team - All Rights Reserved
-
     Team Members: 
     RAYMOND ACEVEDO
     SHAWN WEINER
     CHRISTOPHER SALAZAR
     ROBERT PILLITTERI
     SHELTON LACY 
-
     Unauthorized copying of this file, via any medium is strictly prohibited
     Proprietary and confidential
 */
@@ -23,6 +21,30 @@ module.exports = (sequelize, type) => {
             autoIncrement: true,
             primaryKey: true
         },
+
+    //category: practice(false, 0) or graded(true, 1)
+    category: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+    },
+
+    //level: difficulty of test i.e. 1+, 2+, 3+, etc.
+    level: {
+        type: Sequelize.STRING(50),
+        allowNull: false
+    },
+    
+    //attempts_remaining: number of attempts left to take test
+    attempts_remaining: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+
+    teacherId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
 
         // implement a data field to store data for testing get/post
         data: Sequelize.TEXT
