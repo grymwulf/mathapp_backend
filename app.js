e75c99aef18540bc48d198d8daebd61ceec966f8
grymwulf <shawn@grymwulf.com>
2020-03-02 00:28:07 -0700

    Re-introduce API validation
    Set up API incoming validation - outgoing still suspended

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 /*
-    Copyright 2019 SER401 Project 14 Team - All Rights Reserved
+Copyright 2019 SER401 Project 14 Team - All Rights Reserved
 
-    Team Members: 
-    RAYMOND ACEVEDO
-    SHAWN WEINER
-    CHRISTOPHER SALAZAR
-    ROBERT PILLITTERI
-    SHELTON LACY 
+Team Members: 
+RAYMOND ACEVEDO
+SHAWN WEINER
+CHRISTOPHER SALAZAR
+ROBERT PILLITTERI
+SHELTON LACY 
 
-    Unauthorized copying of this file, via any medium is strictly prohibited
-    Proprietary and confidential
+Unauthorized copying of this file, via any medium is strictly prohibited
+Proprietary and confidential
 */
 
 
@@ -30,73 +30,69 @@ const YAML = require('yamljs');
 const swaggerDocs = YAML.load('./mathapp.apiDocs.yaml');
 const router = express.Router();
 
-require('dotenv').config();
+// OpenApiValidator required modules
+const OpenApiValidator = require('express-openapi-validator').OpenApiValidator;
+const bodyParser = require('body-parser');
+const logger = require('morgan');
 
 // load & Parse api spec
-const apiSpec = YAML.load('./mathapp.apiDocs.yaml');
-
+const apiSpecJSON = YAML.load('./mathapp.apiDocs.yaml');
 
 // define true constants
 
 const SERVER_PORT = process.env.PORT || 8000;
 
 /*
-    Middleware
+Middleware
 */
+// body parser is supposed to be called first, don't recall why
+app.use(bodyParser.json());
+app.use(logger('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 // express settings
 app.set('trust proxy', true);
 
-// initialize app settings
-app.use(express.json());
+// Bypass routing
+app.use('/apptest', apptest);
 
 /*
-// troubleshooting
-app.use(function (req,res,next) {
-    console.log(req.url);
-    console.log(req.ip);
-    next();
-});
+Routes
 */
-
-/*
-    Routes
-*/
-
 router.use('/students', students);
 router.use('/teachers', teachers);
 router.use('/tests', tests);
 router.use('/results', results);
-router.use('/apptest', apptest);
 router.use('/', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
-app.use('/api/v1', router);
-app.use('/', router);
-
+// process routing through OpenApiValidator
+new OpenApiValidator({
+    apiSpec: './mathapp.apiDocs.yaml',
+}).install(app)
+.then(() => {
+    // App use bypass routing
+    app.use('/api/v1', router);                
+    app.use('/', router);
+})
 
 /*
-    Default base route
+Error handler
 */
-// default handler
-// anything not implemented gets a response not implemented
-// this HAS to be last in file to ensure it doesn't trigger on anything else that might match
-app.use(function(req,res) {
-    var result = {};
-    result['data'] = {};
-    result['responseCode'] = HttpStatus.NOT_IMPLEMENTED;
-    result['response'] = "Not Implemented";
-    res.status(result.responseCode);
-    res.json(result);
-    return;
+app.use((err, req, res, next) => {
+    res.status(err.status).json({
+        errors: err.errors,
+    });
 });
 
 
 
-
 /*
-    Server
+Server
 */
 // force ipv4
 // app.listen(SERVER_PORT, '0.0.0.0', () => console.log(`Starting server, listening on ${SERVER_PORT}`));
 
 // ipv6 compatible
-app.listen(SERVER_PORT, () => console.log(`Starting server, listening on ${SERVER_PORT}`));
+app.listen(SERVER_PORT, () => 
+    console.log(`Starting server, listening on ${SERVER_PORT}`));
