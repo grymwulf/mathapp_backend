f6a3ad8e7b5e974f8a792be8463217f2b7bb4f52
Christopher Salazar <csalaz14@asu.edu>
2020-03-17 15:18:22 -0700

    Task 157: Retrieve tests by level category then attempts remaining

--- routes/tests.js
+++ routes/tests.js
@@ -601,6 +601,65 @@ router.get('/level/:level', function (req, res) {
 });
 
 /**
+ * @api (get) /tests/level/:level/category/:category/attemptsRemaining/:attemptsRemaining
+ * 
+ * @apiName GetTestsByLevel&Category&AttemptsRemaining
+ * 
+ * @apiGroup Tests
+ * 
+ * @apiParam (Number) input Test batch Level Category AttemptsRemaining to pull
+ * 
+ * @apiSuccess (JSON) data Current table entry for test
+ * @apiSuccess (JSON) responseCode HTTP Response Code
+ * @apiSuccess (JSON) response Server Response
+ * 
+ * @apiError (JSON) data Empty data set test on error
+ * @apiError (JSON) responseCode HTTP Response Code
+ * @apiError (JSON) response Server Response
+ */
+
+router.get('/level/:level/category/:category/attemptsRemaining/:attemptsRemaining', function (req, res) {
+    var result = {};
+    var paramLevel = req.params.level.split('');
+    var value = req.params.category;
+    if (value === 'true') value = true;
+    if (value === 'false') value = false;
+    data.Test.findAll({
+        where: {
+            baseNumber: paramLevel[0],
+	        operation: paramLevel[1],
+            category: value,
+            attemptsRemaining: req.params.attemptsRemaining
+        }
+    })
+        .then(testData => {
+            var parsed = JSON.parse(JSON.stringify(testData));
+            for(i = 0; i < parsed.length; i++) {
+                delete parsed[i].operation;
+                delete parsed[i].baseNumber;
+            }
+            result['data'] = parsed;
+            result['endpoint'] = `/tests/level/:level/category/:category/attemptsRemaining/:attemptsRemaining`;
+            result['responseCode'] = HttpStatus.OK;
+            result['response'] = "Query Successful";
+            res.status(result.responseCode);
+            res.json(result);
+            return;
+        }).catch(function (err) {
+            console.log('Error querying a test');
+            console.log(err)
+            result['data'] = {};
+            result['endpoint'] = `/tests/level/:level/category/:category/attemptsRemaining/:attemptsRemaining`;
+            result['responseCode'] = HttpStatus.INTERNAL_SERVER_ERROR;
+            result['response'] = "Internal Server Error";
+            res.status(result.responseCode);
+            res.json(result);
+            return;
+        })
+
+});
+
+/**
  * @api (get) /tests/level/:level/students/studentId
  * 
  * @apiName GetTestsByLevel&StudentId
